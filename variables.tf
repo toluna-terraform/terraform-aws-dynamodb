39152d07c217b19392fe49dc0bf98bb5097f9e9f
ShakedYosef <85097819+ShakedYosef@users.noreply.github.com>
2021-08-25 12:51:46 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,29 @@
 variable "env_name" {
-  default = "trn"
+  type = string
 }
 
 variable "table_name" {
   type = string
-  default = "ServiceQuotas"
 }
 
 variable "primary_key"{
     type = string
-    default = "TemplateId"
 }
 
 variable "primary_key_type"{
     type = string
-    default = "S"
 }
 
 variable "primary_sort_key" {
-  default = "Entity"
+  type = string
 }
 
 variable "primary_sort_key_type" {
   type = string
-  default = "N"
 }
 
 variable "secondary_index_name" {
   type = string
-  default = "Entity-index"
 }
 
 variable "read_capacity" {
