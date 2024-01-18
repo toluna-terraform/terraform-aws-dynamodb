13312a453028d721d5751eb54d5654465cbca006
Evgeny Gigi <evgeny.gigi@toluna.com>
2024-01-18 11:33:03 +0200

    removing ttl variables

--- variables.tf
+++ variables.tf
@@ -101,14 +101,3 @@ variable "global_secondary_indeces" {
   description = "Array of GSI definitions"
   default = []
 }
-
-variable "ttl_value" {
-  type = bool
-  default = false 
-  description = "time to leave"
-}
-
-value "ttl_attribute_name" {
-  type = string
-  default = ""
-}
